47cd43c36a8a7160a317b3f552be0062173a07bb
KristofferKristiansen <mattiskk@hiof.no>
2025-04-08 12:16:31 +0200

    oppdaterte header og sanity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,10 @@ function Header() {
       <h1>Gruppe 5</h1>
       <nav style={{ display: 'flex', gap: '1rem' }}>
         <Link to="/">Hjem</Link>
-        <Link to="/member/ID1">Anna</Link>
-        <Link to="/member/ID2">Jonas</Link>
-        <Link to="/member/ID3">Sara</Link>
+        <Link to="/member/ID1">Frida</Link>
+        <Link to="/member/ID2">Kristoffer</Link>
+        <Link to="/member/ID3">Kiur</Link>
+        <Link to="/member/ID4">Kevin</Link>
         {/* Disse ID-ene må byttes med faktiske sanity-id-er etter hvert */}
       </nav>
     </header>
