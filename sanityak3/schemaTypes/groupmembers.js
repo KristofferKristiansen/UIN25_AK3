785f7172767fc1b456afbbaac76d34cb90cde310
KristofferKristiansen <mattiskk@hiof.no>
2025-04-08 10:53:06 +0200

    Created Groupmembers

---
+++ sanityak3/schemaTypes/groupmembers.js
@@ -0,0 +1,27 @@
+export const groupmembers = {
+    name: 'groupmembers',
+    title: 'Group Members',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Frida',
+            type: 'string',
+        },
+        {
+            name: 'name',
+            title: 'Kristoffer',
+            type: 'string',
+        },
+        {
+            name: 'name',
+            title: 'Kevin',
+            type: 'string',
+        },
+        {
+            name: 'name',
+            title: 'Kiur',
+            type: 'string',
+        },
+    ]
+}
