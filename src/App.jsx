24fe0959831c39165e73e9d7872de08b331d91ae
KristofferKristiansen <mattiskk@hiof.no>
2025-04-08 11:09:49 +0200

    Oppdaterte app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Layout>
-        <Routes>
-          <Route></Route>
-        </Routes>
-      </Layout>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/member/:id" element={<Profile />} />
+      </Routes>
     </>
   )
 }
