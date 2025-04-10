a60c6a35b2b57059f8d37aa7f294959641982b49
KristofferKristiansen <mattiskk@hiof.no>
2025-04-10 09:19:51 +0200

    oppdaterte profilkortene

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function ProfileCard({ member }) {
   return (
-    <div style={{ border: '1px solid #ccc', padding: '1rem', width: '200px' }}>
+    <div style={{ border: '2px solid #ccc', padding: '1rem', width: '250px', margin: '1rem' }}>
       <img src={member.imageUrl} alt={member.name} style={{ width: '100%' }} />
       <h3>{member.name}</h3>
       <p>{member.email}</p>
