65d970653a8991ef82b378d8da12df30e592a946
KristofferKristiansen <mattiskk@hiof.no>
2025-04-10 09:43:52 +0200

    endret rammer

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function ProfileCard({ member }) {
   return (
-    <div style={{ border: '2px solid #ccc', padding: '1rem', width: '250px', margin: '1rem' }}>
+    <div style={{ border: '3px solid #ccc', padding: '1rem', width: '250px', margin: '1rem' }}>
       <img src={member.imageUrl} alt={member.name} style={{ width: '100%' }} />
       <h3>{member.name}</h3>
       <p>{member.email}</p>
